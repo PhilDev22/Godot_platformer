2f1b6c669481ce9cd066f21fa0811536c3d7c1ed
Phil <codeydev@gmail.com>
2019-02-16 16:49:09 +0100

    Refactored enemy movement animation

---
+++ Scenes/EnemyGhost.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	pass
+
+func flip_horizontal():
+	$Sprite.flip_h = !$Sprite.flip_h
